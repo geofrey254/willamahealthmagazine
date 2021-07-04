4f41c725479718815d1723d8b2295a698372f149
geofrey254 <devongeoffreymaina@gmail.com>
2021-07-04 23:11:45 +0300

    Postgres DB

--- its_maya/settings.py
+++ its_maya/settings.py
@@ -84,10 +84,24 @@ WSGI_APPLICATION = 'its_maya.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+if DEBUG:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'willamaDB',
+        'USER': 'willamaadmin',
+        'PASSWORD': '@Geoffrey254',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
