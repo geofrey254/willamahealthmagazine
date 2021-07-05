c8e5422aace09e982304add3a9a0aa64b005e602
geofrey254 <devongeoffreymaina@gmail.com>
2021-07-05 10:53:42 +0300

    debug url set

--- blog/urls.py
+++ blog/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('the_savanna_wave/category/<slug:slug>/', views.CategoryView, name='blog_category'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG: 
+        urlpatterns += static(settings.MEDIA_URL, 
+                              document_root=settings.MEDIA_ROOT)
--- its_maya/urls.py
+++ its_maya/urls.py
@@ -38,4 +38,6 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG: 
+        urlpatterns += static(settings.MEDIA_URL, 
+                              document_root=settings.MEDIA_ROOT)
