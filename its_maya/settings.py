65249fd6f02c5a8527853821a7a049321485d7ec
geofrey254 <devongeoffreymaina@gmail.com>
2021-07-10 15:02:32 +0300

    base dir changes

--- its_maya/settings.py
+++ its_maya/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'its_maya.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
