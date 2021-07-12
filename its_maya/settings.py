e6d3ffd11bff623107cd2c86b0ea293072dceed6
geofrey254 <devongeoffreymaina@gmail.com>
2021-07-12 12:06:19 +0300

    Secret key redefined

--- its_maya/settings.py
+++ its_maya/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-8*28(np6ju$p2u@+^$(n0xx=6kftx$e$!i^1cx+wlqwl*jbm^x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
